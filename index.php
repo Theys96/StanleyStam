4a62b5d81a7490da5308954b1b6892b34d82c8ec
Theys96 <accounts@thijshavinga.nl>
2018-02-08 22:48:57 +0100

    Orginal

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+require_once('php/db.php');
+require_once('php/functions.php');
+require_once('php/model.php');
+require_once('php/auth.php');
+$Auth = new Auth($con);
+$Model = new Model($con);
+require_once('php/router.php');
+$Auth->autoAuth();
+
+if (file_exists('php/code/' . $page . '.php')) {
+	require_once('php/code/' . $page . '.php');
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<!--Info-->
+		<title>Stanleystam</title>
+		<link rel="shortcut icon" type="image/png" href="/logo.png"/>
+		<link rel="manifest" type="application/json" href="/manifest.json">
+		<meta charset="UTF-8"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="#222A0A">
+		<meta name="theme-color" content="#222A0A" />
+
+		<!--Resources-->
+		<link rel="stylesheet" type="text/css" href="/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="/style.css">
+		<script src="jquery.js"></script>
+		<?php
+		if (isset($_COOKIE['style'])) {
+			echo '<link rel="stylesheet" type="text/css" href="/style_'.$_COOKIE['style'].'.css">';
+		} else {
+			echo '<link rel="stylesheet" type="text/css" href="/style_dark.css">';
+		}
+		echo "\n";
+		?>
+	</head>
+	<body>
+		<div class="container-fluid">
+			<div><h1 class='text-center py-3'>Stanleystam</h1></div>
+			<div class="row">
+				<div class="col-sm-4">
+						
+					<div class="card my-3">
+						<?php
+						menu($page, $Auth);
+						?>
+					</div>
+
+				</div>
+	  			<div class="col-sm-8">
+
+					<?php
+					if (file_exists('php/page/' . $page . '.php')) {
+						require_once('php/page/' . $page . '.php');
+					}
+					?>
+
+	  			</div>
+			</div>
+		</div>
+	</body>
+</html>
